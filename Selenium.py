0c3263380476a75c3569b1744200de339737a294
Joey <54256112+xjasterx@users.noreply.github.com>
2022-11-02 20:09:46 -0400

    Added Selenium scripting file

---
+++ Selenium.py
@@ -0,0 +1,14 @@
+# import webdriver
+from selenium import webdriver
+
+# create webdriver object
+driver = webdriver.Firefox()
+
+# get geeksforgeeks.org
+driver.get("https://www.geeksforgeeks.org/")
+
+# get element
+element = driver.find_element_by_link_text("abrakadabra")
+
+# click the item
+print(element.click())
